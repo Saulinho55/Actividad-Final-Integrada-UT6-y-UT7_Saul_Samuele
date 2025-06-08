ed09c3c6d57f3fbea0a235904ba732d9c5b4ce56
Saulinho55 <Saulespino555@gmail.com>
2025-06-08 23:22:36 +0100

    Spring Boot Creado
    Spring Boot Creado

---
+++ notas/src/test/java/com/api/notas/NotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
